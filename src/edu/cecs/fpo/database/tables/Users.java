cee5e4b3d25c8a777a7c68142153d1de4c385683
Nick <nickar@knights.ucf.edu>
2014-02-23 23:36:38 -0500

    Removed Hibernate Files in favor of using JDBC.

--- src/edu/cecs/fpo/database/tables/Users.java
+++ src/edu/cecs/fpo/database/tables/Users.java
@@ -10,4 +10,83 @@ package edu.cecs.fpo.database.tables;
  */
 public class Users {
 
+	/** 
+	 * PRIMARY KEY - The user's identification number
+	 * */
+	int userId;
+
+	/** The user's username */
+	String username;
+
+	/** The user's password */
+	String password;
+
+	/** The user's first name */
+	String firstName;
+
+	/** The user's last name*/
+	String lastName;
+
+	/** The user's email address */
+	String emailAddress;
+
+	/** The user's role */
+	String role;
+	
+	public int getUserId(){
+		return this.userId;
+	}
+	
+	public void setUserId(int userId){
+		this.userId = userId;
+	}
+	
+	public String getUsername(){
+		return this.username;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+	
+	public String getFirstName(){
+		return this.firstName;
+	}
+	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	
+	public String getLastName(){
+		return this.firstName;
+	}
+	
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	
+	public String getEmailAddress(){
+		return this.emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress){
+		this.emailAddress = emailAddress;
+	}
+	
+	public String getRole(){
+		return this.role;
+	}
+	
+	public void setRole(String role){
+		this.role = role;
+	}
+
 }
